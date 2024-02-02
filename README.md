0321ce8b83d8ce903470ff351320b4ceca1e9ce9
j32u4ukh <j32u4ukh@gmail.com>
2024-02-02 23:39:26 +0800

    feat: webpack 編譯 SCSS

---
+++ README.md
@@ -0,0 +1,5 @@
+執行 Webpack 編譯
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dst"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+};
